1d9bba89c27605f89ec5f1ca4261074b1d3efb64
anish <anishkr545@gmail.com>
2023-02-20 11:25:12 +0530

    implememented Dynamic filtering

--- src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -3,16 +3,29 @@ package com.rest.webservices.restfulwebservices.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class FilteringController {
 
 	@GetMapping("/filtering")
-	public SomeBean filtering() {
+	public MappingJacksonValue filtering() {
 	
-		return new SomeBean("win","achieve","keep moving");
+		SomeBean someBean = new SomeBean("win","achieve","keep moving");
+		MappingJacksonValue mappingJacksonValue=new MappingJacksonValue(someBean);
+		
+		SimpleBeanPropertyFilter filter=SimpleBeanPropertyFilter.filterOutAllExcept("field1");
+		FilterProvider filters=new SimpleFilterProvider().addFilter("Somebean",filter);
+		mappingJacksonValue.setFilters(filters);
+		
+		
+		return mappingJacksonValue;
 	}
 	
 	@GetMapping("/filtering-list")
